e582c984908479af7251bccda364fd863e9b1a76
Wojciech Sierota <wojciech.sierota.s@gmail.com>
2026-03-26 19:35:00 +0100

    Add DatabaseManager class for stateful SQL connection management:

---
+++ database_manager.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self, database):
+        self.database = database
+        self.con = None
+
+    def connect(self):
+        self.con = sqlite3.connect(self.database)
+
+    def disconnect(self):
+        if self.con:
+            self.con.close()
+
+    def execute_query(self, query, params=None):
+        cur = self.con.cursor()
+        cur.execute(query, params or ())
+        self.con.commit()
+
+    def fetch_all(self, query, params=None):
+        cur = self.con.cursor()
+        cur.execute(query, params or ())
+        return cur.fetchall()
+    
+    
